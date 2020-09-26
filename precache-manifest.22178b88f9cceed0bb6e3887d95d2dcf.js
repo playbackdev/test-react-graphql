20e07b5650a1f850af61c336cef3efb8b1b7e718
German <shvetsov.g@gmail.com>
2020-09-26 17:49:01 +0300

    Updates

---
+++ precache-manifest.22178b88f9cceed0bb6e3887d95d2dcf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "860a25e1306823a7755a53b7c2c6d024",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d8b8b688da955010b5f3",
+    "url": "/static/css/main.25d6df6f.chunk.css"
+  },
+  {
+    "revision": "78df6a463be4f0d58c7b",
+    "url": "/static/js/2.a8678ce5.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.a8678ce5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8b8b688da955010b5f3",
+    "url": "/static/js/main.c65e0da1.chunk.js"
+  },
+  {
+    "revision": "cb0bf48ed4493f43efc8",
+    "url": "/static/js/runtime-main.306fc074.js"
+  },
+  {
+    "revision": "7af4ad35f6eb60fc8f2f05fdfe91b90a",
+    "url": "/static/media/user-icon.7af4ad35.png"
+  }
+]);
